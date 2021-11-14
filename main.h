5f56ab0b740f0e2bc03990527231c14990f7bfc8
Stanley Serbin <stanleyserbin@gmail.com>
2021-11-14 18:25:09 +0100

    norm ok

--- main.h
+++ main.h
@@ -18,6 +18,13 @@
 # include <stdlib.h>
 # include <string.h>
 
+typedef struct s_loop{
+	int				i;
+	char			**to_return;
+	unsigned int	x;
+	unsigned int	last_index;
+}				t_loop;
+
 unsigned int	count_len_matrice(char **matrice);
 unsigned int	count_len_next(char *str);
 void			read_that_matrice(char **matrice);
@@ -25,5 +32,6 @@ unsigned int	count_occ(char *str, char occ);
 char			*ft_substr(char *str, unsigned int start, unsigned int end);
 char			**push_arr(char **nbr, char *to_add);
 void			free_that_matrice(char **matrice);
+char			**parse_argv(int argc, char **argv, char **nbr);
 
 #endif
---
+++ src/parse_argv.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_argv.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sserbin <stanleyserbin@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/14 14:50:48 by sserbin           #+#    #+#             */
+/*   Updated: 2021/11/14 14:50:49 by sserbin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../main.h"
+
+char	**get_to_return(unsigned int size)
+{
+	char	**to_return;
+
+	to_return = malloc(sizeof(char *) * size);
+	if (!to_return)
+	{
+		printf("Error while allocating pointer on get single arg");
+		return (NULL);
+	}
+	return (to_return);
+}
+
+char	*check_error_single_arg(char **to_return, unsigned int x)
+{
+	if (!to_return[x])
+	{
+		free_that_matrice(to_return);
+		return (NULL);
+	}
+	if (!to_return[x][0])
+	{
+		free(to_return[x]);
+		to_return[x] = 0;
+	}
+	return (to_return[x]);
+}
+
+t_loop	ft_loop(t_loop loop, char *str)
+{
+	if (str[loop.i] == ' ')
+	{
+		loop.to_return[++loop.x] = ft_substr(str, loop.last_index, loop.i);
+		if (!loop.to_return[loop.x])
+		{
+			free_that_matrice(loop.to_return);
+			loop.to_return = NULL;
+			return (loop);
+		}
+		loop.last_index = loop.i + 1;
+	}
+	while (str[loop.i] == ' ')
+		loop.i++;
+	return (loop);
+}
+
+char	**get_single_arg(char *str)
+{
+	int				i;
+	char			**to_return;
+	int				x;
+	unsigned int	last_index;
+	t_loop			loop;
+
+	loop.i = -1;
+	loop.x = -1;
+	loop.last_index = 0;
+	loop.to_return = get_to_return(count_occ(str, ' ') + 2);
+	if (!loop.to_return)
+		return (NULL);
+	while (str[++loop.i])
+		loop = ft_loop(loop, str);
+	loop.to_return[++loop.x] = ft_substr(str, loop.last_index, loop.i);
+	loop.to_return[loop.x] = check_error_single_arg(loop.to_return, loop.x);
+	loop.to_return[++loop.x] = 0;
+	return (loop.to_return);
+}
+
+char	**parse_argv(int argc, char **argv, char **nbr)
+{
+	unsigned int	i;
+	char			**tmp;
+	unsigned int	x;
+
+	i = 1;
+	while (i < argc)
+	{
+		tmp = get_single_arg(argv[i]);
+		if (!tmp)
+		{
+			free_that_matrice(nbr);
+			exit(0);
+		}
+		x = 0;
+		while (tmp[x])
+		{
+			nbr = push_arr(nbr, tmp[x]);
+			if (!nbr)
+				exit(0);
+			x++;
+		}
+		free(tmp);
+		i++;
+	}
+	return (nbr);
+}
